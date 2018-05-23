f3545f1e89cee4c85c03dd149caa45792665067f
Dennis Beatty <dennis@dcbeatty.com>
2018-05-23 00:26:41 -0600

    log out to syslog

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule CampWithDennis.Mixfile do
     [
       {:cowboy, "~> 1.0"},
       {:distillery, "~> 1.5"},
+      {:ex_syslogger, "~> 1.4"},
       {:gettext, "~> 0.11"},
       {:httpoison, "~> 1.1"},
       {:phoenix, "~> 1.3.0"},
